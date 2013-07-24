fcc5c423f7a173cae94c1ea42cf1fb5bbb497d1a
Geoff Greer <geoff@greer.fm>
2013-07-24 01:46:27 -0700

    Add build and deploy scripts

---
+++ build_release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DATETIME=`date -u '+%Y_%m_%d_%H%M'`
+RELEASE_NAME="news-$DATETIME"
+
+jekyll build && \
+echo "$RELEASE_NAME.tar.gz" && \
+tar -c -z -f $RELEASE_NAME.tar.gz _site
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -ne 2 ] && [ $# -ne 1 ]
+then
+  echo "Usage: $0 release-name.tar.gz hostname"
+  exit 0
+fi
+
+if [ $# -eq 1 ]
+then
+  TARBALL=`./build_release.sh`
+  HOST=$1
+else
+  TARBALL=$1
+  HOST=$2
+fi
+
+RELEASE_NAME=$(basename "$TARBALL")
+RELEASE_NAME="${RELEASE_NAME%%.*}"
+RELEASE_DIR="/data/releases/$RELEASE_NAME"
+
+echo "Deploying $RELEASE_NAME to $HOST"
+
+scp -C $TARBALL $HOST:/tmp
+
+ssh $HOST "sudo mkdir $RELEASE_DIR && \
+sudo tar xzf /tmp/$RELEASE_NAME.tar.gz --directory $RELEASE_DIR"
+ssh $HOST "sudo ln -s -f $RELEASE_DIR /data/news-new && \
+sudo mv -T -f /data/news-new /data/news"
