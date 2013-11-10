23ea90b3bbaee3b20a1bd6e62e708864ad90504d
Geoff Greer <geoff@greer.fm>
2013-11-09 16:43:42 -0800

    Multi-deploy script for news. Build script only prints out the name, not lots of other junk.

--- build_release.sh
+++ build_release.sh
@@ -3,6 +3,6 @@
 DATETIME=`date -u '+%Y_%m_%d_%H%M'`
 RELEASE_NAME="news-$DATETIME"
 
-jekyll build && \
+jekyll build > /dev/null && \
 echo "$RELEASE_NAME.tar.gz" && \
 tar -c -z -f $RELEASE_NAME.tar.gz -C _site .
--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
-if [ $# -ne 2 ] && [ $# -ne 1 ]
+if [ $# -eq 0 ]
 then
-  echo "Usage: $0 release-name.tar.gz hostname"
+  echo "Usage: $0 release-name.tar.gz hostname0 hostname1 hostname2 ..."
   exit 0
 fi
 
-if [ $# -eq 1 ]
+echo $1 | grep '\.tar\.gz$'
+
+if [ $? -eq 0 ]
 then
-  ./build_release.sh
-  TARBALL=`ls -t *.tar.gz | head -n 1`
-  HOST=$1
-else
   TARBALL=$1
-  HOST=$2
+  shift
+else
+  echo "Building tarball..."
+  TARBALL=`./build_release.sh`
+  echo "Built $TARBALL"
 fi
 
 RELEASE_NAME=$(basename "$TARBALL")
 RELEASE_NAME="${RELEASE_NAME%%.*}"
 RELEASE_DIR="/data/releases/$RELEASE_NAME"
 
-echo "Deploying $RELEASE_NAME to $HOST"
+for HOST in $@
+do
+  echo "Deploying $RELEASE_NAME to $HOST"
 
-scp -C $TARBALL $HOST:/tmp
+  scp -C $TARBALL $HOST:/tmp
 
-ssh $HOST "sudo mkdir $RELEASE_DIR && \
-sudo tar xzf /tmp/$RELEASE_NAME.tar.gz --directory $RELEASE_DIR"
-ssh $HOST "sudo ln -s -f $RELEASE_DIR /data/news-new && \
-sudo mv -T -f /data/news-new /data/news"
+  ssh $HOST "sudo mkdir $RELEASE_DIR && \
+    sudo tar xzf /tmp/$RELEASE_NAME.tar.gz --directory $RELEASE_DIR"
+  ssh $HOST "sudo ln -s -f $RELEASE_DIR /data/news-new && \
+    sudo mv -T -f /data/news-new /data/news"
+
+  if [ $? -eq 0 ]
+  then
+    curl -X POST http://$USER:$USER@dev00.floobits.com/deploy/django/$HOST
+  else
+    echo "OMG DEPLOY FAILED"
+  fi
+done
